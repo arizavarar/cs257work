edc3f8227e131897f14a3320e3f8e90d7e1ed3ac
arizavarar <arizavarar@carleton.edu>
2024-04-08 02:22:32 -0500

    updated table formatting

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,18 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
   quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
+  magType text,
+  rms real,
+  net text,
   id text,
+  updated time with time zone,
   place text,
-  quaketype text
+  quaketype text,
+  status text,
+  location text,
+  magSource text,
 );
