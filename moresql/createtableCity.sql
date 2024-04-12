a0feb59a9b34ffce934393c648c6e88e434ed536
arizavarar <arizavarar@carleton.edu>
2024-04-12 10:08:28 -0500

    Some more format fixes to sql file

--- moresql/createtableCity.sql
+++ moresql/createtableCity.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS us_city_pop;
-CREATE TABLE us-city-pop (
+CREATE TABLE us_city_pop (
 city text,
 city_state text,
 city_population  real,
--- moresql/createtableState.sql
+++ moresql/createtableState.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS us_state_pop;
-CREATE us-state-pop (
+CREATE us_state_pop (
 code text,
 state text,
 population real
