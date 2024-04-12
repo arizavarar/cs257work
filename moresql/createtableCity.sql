cb6a8bf0ddbeac82413f9314b5d6eff9235f46b2
arizavarar <arizavarar@carleton.edu>
2024-04-12 09:54:52 -0500

    added createtable sql files

---
+++ moresql/createtableCity.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXIST us-city-pop;
+CREATE TABLE us-city-pop(
+city text,
+city_state text,
+city_population  real,
+latitude real,
+longitude real
+);
---
+++ moresql/createtableState.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXIST us-state-pop;
+CREATE us-state-pop (
+code text,
+state text,
+population real
+);
