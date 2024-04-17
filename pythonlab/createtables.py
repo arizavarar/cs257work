57f98354886ac42579c581537aa87adf59367bba
arizavarar <arizavarar@carleton.edu>
2024-04-17 10:32:41 -0500

    updated createtables.py kfk ;
    djiovjf;;D
    :

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,15 +1,32 @@
-DROP TABLE IF EXISTS us_state_pop;
-CREATE TABLE us_state_pop (
-code text,
-state text,
-population real
-);
+import psycopg2
+def createTables():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="arizavarar",
+        user="arizavarar",
+        password="expo795beach")
 
-DROP TABLE IF EXISTS us_city_pop;
-CREATE TABLE us_city_pop (
-city text,
-city_state text,
-city_population  real,
-latitude real,
-longitude real
-);
+    cur = conn.cursor()
+
+    sql = """    DROP TABLE IF EXISTS us_state_pop;
+    CREATE TABLE us_state_pop (
+    code text,
+    state text,
+    population real
+    );
+
+    DROP TABLE IF EXISTS us_city_pop;
+    CREATE TABLE us_city_pop (
+    city text,
+    city_state text,
+    city_population  real,
+    latitude real,
+    longitude real
+    );"""
+    
+    cur.execute( sql )
+
+    conn.commit()
+    cur.close()
+
