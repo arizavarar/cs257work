a21de4e258ba59a3b26d9886e6bd8ea019e83d8b
arizavarar <arizavarar@carleton.edu>
2024-04-22 18:54:46 -0500

    testing out new functions

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -77,6 +77,82 @@ def checkSmallestCityMN():
     
     return print("This is the smallest city in MN: " + smallestCity)
 
+def citiesFurthestInEachdirection():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="arizavarar",
+        user="arizavarar",
+        password="expo795beach")
+
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM us_city_pop2"
+    
+    cur.execute(sql)
+
+    rows = cur.fetchall()
+
+    northSpot = rows[0][3]
+    northSpotName = ""
+    southSpot = rows[0][3]
+    southSpotName = ""
+    eastSpot = rows[0][4]
+    eastSpotName = ""
+    westSpot = rows[0][4]
+    westSpotName = ""
+
+    for row in rows:
+        if row[3] > northSpot:
+            northSpot = row[3]
+            northSpotName = row[0]
+    for row in rows:
+        if southSpot >= row [3]:
+            southSpot = row[3]
+            southSpotName = row[0]
+    for row in rows:
+        if row[4] > eastSpot:
+            eastSpot = row[4]
+            eastSpotName = row[0]
+    for row in rows:
+        if westSpot >= row[4]:
+            westSpot = row[4]
+            westSpotName = row[0]
+    
+    stringCities = "These are the cities at the furthest spot in each direction "
+    return print(stringCities + "\n" "North: " + northSpotName + "South: " + southSpotName + "East: " + eastSpotName + "West: " + westSpotName)
+
+def printOutCityPopulationTotal():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="arizavarar",
+        user="arizavarar",
+        password="expo795beach")
+
+    cur = conn.cursor()
+
+    sql = """SELECT city,city_state, city_population, code FROM us_city_pop2 JOIN us_state_pop2 
+    on state = city_state WHERE code = %s OR city_state = %s"""
+    
+    userInput = input("Type a states name or abbreviation to obtain its cities population total: ")
+
+    cur.execute(sql, (userInput,userInput))
+
+    rows = cur.fetchall()
+
+    combinedCityPop: 0
+    nameOfState = ""
+
+    for row in rows:
+        combinedCityPop += row[2]
+        nameOfState = row[1]
+
+    return print("This is the combined population for all the cities in " + nameOfState + ": " + combinedCityPop)
+
+
 checkForNorthfield()
 printLargestCityName()
 checkSmallestCityMN()
+citiesFurthestInEachdirection()
+printOutCityPopulationTotal()
