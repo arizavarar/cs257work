a903ec17727aed2a8a80fa98a760cb6c1d538731
arizavarar <arizavarar@carleton.edu>
2024-05-02 21:34:41 -0500

    fixed some bugs

--- flasklab/webAppFlask.py
+++ flasklab/webAppFlask.py
@@ -13,30 +13,30 @@ def welcome():
 def getValue():
     name = request.form['fname']
     state = request.form['state']
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="arizavarar",
-        user="arizavarar",
-        password="expo795beach")
-
-    cur = conn.cursor()
-
-    sql = """SELECT city,city_state, city_population, code FROM us_city_pop2 JOIN us_state_pop2 
-    on state = city_state WHERE code = %s OR city_state = %s"""
-    
-    userInput = input(state)
-
-    cur.execute(sql, (userInput,userInput))
-
-    rows = cur.fetchall()
-
-    combinedCityPop = 0
-
-    for row in rows:
-        combinedCityPop += row[2]
-    print(name)
-    return render_template("stateChosen.html", n=name, st=state, cityPop=str(combinedCityPop))
+    #conn = psycopg2.connect(
+    #    host="localhost",
+    #    port=5432,
+    #    database="arizavarar",
+    #    user="arizavarar",
+    #    password="expo795beach")
+#
+    #cur = conn.cursor()
+#
+    #sql = """SELECT city,city_state, city_population, code FROM us_city_pop2 JOIN us_state_pop2 
+    #on state = city_state WHERE code = %s OR city_state = %s"""
+    #
+    #userInput = input(state)
+#
+    #cur.execute(sql, (userInput,userInput))
+#
+    #rows = cur.fetchall()
+#
+    #combinedCityPop = 0
+#
+    #for row in rows:
+    #    combinedCityPop += row[2]
+    #return print(name)
+    #return render_template("stateChosen.html", n=name, st=state, cityPop=str(combinedCityPop))
 
 if __name__ == '__main__':
     my_port = 5111
