46f50606f2ad374d33d22c3842a8eaee10a39887
arizavarar <arizavarar@carleton.edu>
2024-05-13 10:22:56 -0500

    final testing

--- csslab/static/functionMainWeb.js
+++ csslab/static/functionMainWeb.js
@@ -1,19 +1,16 @@
 the_heading = document.getElementById("webTitle");
 
-the_heading.onclick = function(){ the_heading.innerText="Goodbye World!"};
+the_heading.onclick = function() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+}    
 
 the_paragraph = document.getElementById("paragraph1");
 
 the_paragraph.onmouseenter = function() { 
-	the_paragraph.style.color = "RoyalBlue";
-  the_paragraph.style.backgroundColor = "white";
+    the_paragraph.style.color = "RoyalBlue";
+    the_paragraph.style.backgroundColor = "white";
 };
 
-function changeColor() {
-  text_input_element = document.getElementById("user-color");
-  new_color = text_input_element.value;
-	the_heading.style.color = new_color;
-}
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
