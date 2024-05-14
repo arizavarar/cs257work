e0b61c138eda7905009272176507d1f15368fcf1
arizavarar <arizavarar@carleton.edu>
2024-05-14 03:24:09 -0500

    final testing

--- csslab/mainWeb.py
+++ csslab/mainWeb.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def welcome():
     return render_template("mainWebsite.html")
 
-@app.route('/<brand>/<ram>/<storage>')
+@app.route('/<brand>/int<ram>/<storage>')
 def laptopBrandChosen(brand, ram, storage):
     
     # Establishing Environment
