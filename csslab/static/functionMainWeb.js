f94f866c1c8620726d3e7c8f4b99a8af56969853
arizavarar <arizavarar@carleton.edu>
2024-05-15 09:52:25 -0500

    added new Templates, and testing loading images/text

--- csslab/static/functionMainWeb.js
+++ csslab/static/functionMainWeb.js
@@ -28,16 +28,3 @@ function submitCompiledLink() {
     window.location.href = compiledLink;
 }
 
-// Close the dropdown menu if the user clicks outside of it
-function clickOut(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
