c3dbd6c81fb0482c7a78f5fb532b4e25ee5116d2
arizavarar <arizavarar@carleton.edu>
2024-05-15 01:21:34 -0500

    added new Templates, and testing loading images/text

--- csslab/mainWeb.py
+++ csslab/mainWeb.py
@@ -26,7 +26,7 @@ def laptopBrandChosen(brand, ram, storage):
         intStorage = int(storage)
 
         # Construct the SQL query
-        query = "SELECT Laptop_Name, lID, Price FROM laptops WHERE Brand = %s AND RAM = %s AND Storage = %s;"
+        query = "SELECT Laptop_Name, Price FROM laptops WHERE Brand = %s AND RAM = %s AND Storage = %s;"
         cur.execute(query, (brand, intRam, intStorage))
 
         rows = cur.fetchall()
