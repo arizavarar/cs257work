f9d1b33d78c214e6229baa4c37178f98562be88a
arizavarar <arizavarar@carleton.edu>
2024-05-15 19:53:30 -0500

    added new Templates, and testing loading images/text

--- csslab/static/laptopDisplayerFunctions.js
+++ csslab/static/laptopDisplayerFunctions.js
@@ -1,17 +1,25 @@
-function myFunc() {
-    return brandName + ram
+function onLoadFunct() {
+    // Grab the brand, ram, storage from the url
+    // GET THE json from the Flask server
+    // Call Generate Display
 }
 
-function generateDisplay(){
+function generateDisplay(data){
 
-    //const para =  document.createElement("p");
-    //para.innerText = brandName;
-    //document.getElementById("myDIV").appendChild(para);
+    the_json = data;
+    laptopName = the_json['laptopsName']
+    laptopPrices = the_json['laptopsPrices']
+
+    console.log(laptopName)
+    console.log(laptopPrices)
+
+    /*
     for (let i = 0; i < brandName.size(); i++) {
         const para = document.createElement("p");
         para.innerText = "Name of Laptop: " + brandName[i] + " Ram: " + ramS[i] + " Storage size: " + storageS;
         // Append to body:
         document.getElementById("myDIV").appendChild(para);
       }
+      */
         
 }
