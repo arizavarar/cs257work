a3e264ff37a4da099fa606052f6733bf776aa0f3
arizavarar <arizavarar@carleton.edu>
2024-05-17 11:18:54 -0500

    final testing

--- csslab/mainWeb.py
+++ csslab/mainWeb.py
@@ -67,7 +67,7 @@ def searchFunction(wordSearched):
     
     query = """SELECT Laptop_Name, Price FROM laptops WHERE Brand LIKE %s OR Laptop_Name LIKE %s OR Price LIKE %s 
     OR Processor_Brand LIKE %s OR GPU LIKE %s OR OS LIKE %s;"""""
-    cur.execute(query,(wordSearched,wordSearched ))
+    cur.execute(query,(wordSearched,wordSearched,wordSearched,wordSearched,wordSearched,wordSearched ))
 
     rows = cur.fetchall()
 
