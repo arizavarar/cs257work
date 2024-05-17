8f1614c13101f6c4799a8b451e10fa95f4437c31
arizavarar <arizavarar@carleton.edu>
2024-05-16 21:07:15 -0500

    final testing

--- csslab/static/laptopDisplayerFunctions.js
+++ csslab/static/laptopDisplayerFunctions.js
@@ -1,25 +1,40 @@
 function onLoadFunct() {
-    // Grab the brand, ram, storage from the url
-    // GET THE json from the Flask server
+    // Grab the brand, ram, storage from the URL
+    // GET THE JSON from the Flask server
     // Call Generate Display
-    pathstring = location.pathname;
-    pathlist=pathstring.split('/');
-    brand = pathlist[2];
-    ram = pathlist[3];
-    storage = pathlist[4];
+    const pathstring = location.pathname;
+    const pathlist = pathstring.split('/');
+    const brand = pathlist[2];
+    const ram = pathlist[3];
+    const storage = pathlist[4];
 
-    console.log("onLoad: " + brand + " " + ram + " " + storage)
+    console.log("onLoad: " + brand + " " + ram + " " + storage);
 
-    queryURL = "/json/"+brand+"/"+ram+"/"+storage;
-    fetch(queryURL).then(response => response.json()).then(data => generateDisplay(data));
+    const queryURL = `/json/${brand}/${ram}/${storage}`;
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => generateDisplay(data))
+        .catch(error => console.error('Error fetching data:', error));
 }
 
 function generateDisplay(data) {
-    const laptopNames = data['nameForLaptop'];
-    const laptopPrices = data['priceForLaptop'];
+    if (!data.nameForLaptop || !data.priceForLaptop) {
+        // Display an error message if the expected data is not found
+        const container = document.getElementById("myDIV");
+        const errorMsg = document.createElement("p");
+        errorMsg.innerText = "No laptops found or an error occurred.";
+        container.appendChild(errorMsg);
+        return;
+    }
+
+    const laptopNames = data.nameForLaptop;
+    const laptopPrices = data.priceForLaptop;
 
     console.log(laptopNames);
     console.log(laptopPrices);
+
+    const container = document.getElementById("myDIV");
+    container.innerHTML = ''; // Clear previous contents
 
     for (let i = 0; i < laptopNames.length; i++) {
         let pic = document.createElement('img');
@@ -35,7 +50,6 @@ function generateDisplay(data) {
         pricePara.appendChild(priceText);
 
         // Append to myDIV:
-        const container = document.getElementById("myDIV");
         container.appendChild(pic);
         container.appendChild(nameHeading);
         container.appendChild(pricePara);
